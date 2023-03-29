511fa794da63268e61d30fbdddf12839732d38ae
TopBlokeChase <102511226+TopBlokeChase@users.noreply.github.com>
2023-03-29 17:43:55 +1000

    Import Template Files

---
+++ JobCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+
+
+public class JobCollection : IJobCollection {
+	private IJob[] jobs;
+	private uint count;
+
+	public JobCollection( uint capacity ) {
+		if( !( capacity >= 1 ) ) throw new ArgumentException();
+		jobs = new IJob[capacity];
+        count = 0;
+	}
+
+	public uint Capacity {
+		get { return (uint) jobs.Length; }
+	}
+
+	public uint Count {
+		get { return count; }
+	}
+
+	public bool Add( IJob job ) {
+        //To be implemented by students
+        throw new System.NotImplementedException();
+    }
+
+    public bool Contains(uint id) {
+        //To be implemented by students
+        throw new System.NotImplementedException();
+
+    }
+
+    public IJob? Find( uint id ) {
+        //To be implemented by students
+        throw new System.NotImplementedException();
+
+    }
+
+    public bool Remove(uint id) {
+        //To be implemented by students
+        throw new System.NotImplementedException();
+    }
+
+    public IJob[] ToArray() {
+        //To be implemented by students
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Scheduler.cs
@@ -0,0 +1,30 @@
+﻿
+
+public class Scheduler : IScheduler {
+	public Scheduler( IJobCollection jobs ) {
+		Jobs = jobs;
+	}
+
+	public IJobCollection Jobs { get; }
+
+	public IJob[] FirstComeFirstServed() {
+
+        //To be implemented by students
+        throw new System.NotImplementedException();
+
+    }
+
+    public IJob[] Priority() {
+
+        //To be implemented by students
+        throw new System.NotImplementedException();
+
+    }
+
+    public IJob[] ShortestJobFirst() {
+
+        //To be implemented by students
+        throw new System.NotImplementedException();
+
+    }
+}

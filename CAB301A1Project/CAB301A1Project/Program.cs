64ce53c2897342be8f68b8917ed41580763271f2
TopBlokeChase <102511226+TopBlokeChase@users.noreply.github.com>
2023-03-30 19:17:16 +1000

    Finished main coding + basic interface + variable random testing
    Finished implementation of all methods. Added basic interface on run to test code. Now has 2 test functions; basic explicit test case, and variable randomised dataset. All tests controlled through interface.

--- CAB301A1Project/CAB301A1Project/Program.cs
+++ CAB301A1Project/CAB301A1Project/Program.cs
@@ -9,22 +9,91 @@ namespace CAB301A1Project
             Console.WriteLine("+-------------------+\n" +
                               "| Main Method Start |\n" +
                               "+-------------------+");
-            Console.WriteLine("---Test Starting---");
-            TestSortFCFS();
-            Console.WriteLine("---Test Finished---");
+            Console.WriteLine("Select an option");
+            Console.WriteLine("1: Basic Test\n" +
+                              "2: Randomised Test");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("---Basic Test Starting---");
+                    TestSorter();
+                    Console.WriteLine("---Basic Test Finished---");
+                    break;
+                case "2":
+                    Console.WriteLine("How many entries? (0-999)");
+                    uint amount = uint.Parse(Console.ReadLine());
+                    Console.WriteLine("What sorting type?\n" +
+                                      "1: First-Come, First-Served\n" +
+                                      "2: Priority\n" +
+                                      "3: Shortest Job First");
+                    input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "1":
+                            TestSorterRandomised(amount, "FCFS");
+                            break;
+                        case "2":
+                            TestSorterRandomised(amount, "Priority");
+                            break;
+                        case "3":
+                            TestSorterRandomised(amount, "SJF");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Option");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+            } 
+            
         }
 
-        static void TestSortFCFS()
+        static void TestSorter()
         {
             Job job1 = new Job(1, 1, 5, 9);
             Job job2 = new Job(2, 9, 1, 5);
             Job job3 = new Job(3, 5, 9, 1);
-            JobCollection jobCol1 = new JobCollection(3);
-            jobCol1.Add(job1);
-            jobCol1.Add(job2);
-            jobCol1.Add(job3);
-            Scheduler jobSch = new Scheduler(jobCol1);
-            IJob[] sortedCol1 = jobSch.FirstComeFirstServed();
+            JobCollection jobCol = new JobCollection(3);
+            jobCol.Add(job1);
+            jobCol.Add(job2);
+            jobCol.Add(job3);
+            Scheduler jobSch = new Scheduler(jobCol);
+            jobSch.FirstComeFirstServed();
+            jobSch.Priority();
+            jobSch.ShortestJobFirst();
+        }
+        static void TestSorterRandomised(uint amount, string type)
+        {
+            if (amount < 0 || amount > 999)
+            {
+                Console.WriteLine("Out of range: 0-999");
+                return;
+            }
+            JobCollection jobCol = new JobCollection(amount);
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                jobCol.Add(new Job((uint)i + 1, (uint)random.Next(1, 1000), (uint)random.Next(1, 1000), (uint)random.Next(1, 10)));
+            }
+            Scheduler jobSch = new Scheduler(jobCol);
+            switch (type)
+            {
+                case "FCFS":
+                    jobSch.FirstComeFirstServed();
+                    break;
+                case "Priority":
+                    jobSch.Priority();
+                    break;
+                case "SJF":
+                    jobSch.ShortestJobFirst();
+                    break;
+                default:
+                    Console.WriteLine("Invalid sorting type");
+                    break;
+            }
         }
     }
 }
--- CAB301A1Project/CAB301A1Project/Scheduler.cs
+++ CAB301A1Project/CAB301A1Project/Scheduler.cs
@@ -12,7 +12,8 @@
 
         public IJob[] FirstComeFirstServed()
         {
-            //To be implemented by students
+            Console.WriteLine("Frst-Come, First-Served Sort");
+            //To be implemented by students - Done
             //Do commenting
             //Uses Bubble Sort
             sortedJobs = Jobs.ToArray();
@@ -44,19 +45,69 @@
 
         public IJob[] Priority()
         {
-
+            Console.WriteLine("Priority Sort");
             //To be implemented by students
             //Do commenting
-            throw new System.NotImplementedException();
+            //Uses Bubble Sort
+            sortedJobs = Jobs.ToArray();
+            IJob tempJob;
+            Console.WriteLine("Before Sort");
+            foreach (IJob job in sortedJobs)
+            {
+                Console.WriteLine(job.ToString());
+            }
+            for (int i = 0; i < Jobs.Count - 1; i++)
+            {
+                for (int j = 0; j < Jobs.Count - 1 - i; j++)
+                {
+                    if (sortedJobs[j].Priority < sortedJobs[j + 1].Priority)
+                    {
+                        tempJob = sortedJobs[j];
+                        sortedJobs[j] = sortedJobs[j + 1];
+                        sortedJobs[j + 1] = tempJob;
+                    }
+                }
+            }
+            Console.WriteLine("After Sort");
+            foreach (IJob job in sortedJobs)
+            {
+                Console.WriteLine(job.ToString());
+            }
+            return sortedJobs;
 
         }
 
         public IJob[] ShortestJobFirst()
         {
-
+            Console.WriteLine("Shortest First Sort");
             //To be implemented by students
             //Do commenting
-            throw new System.NotImplementedException();
+            //Uses Bubble Sort
+            sortedJobs = Jobs.ToArray();
+            IJob tempJob;
+            Console.WriteLine("Before Sort");
+            foreach (IJob job in sortedJobs)
+            {
+                Console.WriteLine(job.ToString());
+            }
+            for (int i = 0; i < Jobs.Count - 1; i++)
+            {
+                for (int j = 0; j < Jobs.Count - 1 - i; j++)
+                {
+                    if (sortedJobs[j].ExecutionTime > sortedJobs[j + 1].ExecutionTime)
+                    {
+                        tempJob = sortedJobs[j];
+                        sortedJobs[j] = sortedJobs[j + 1];
+                        sortedJobs[j + 1] = tempJob;
+                    }
+                }
+            }
+            Console.WriteLine("After Sort");
+            foreach (IJob job in sortedJobs)
+            {
+                Console.WriteLine(job.ToString());
+            }
+            return sortedJobs;
 
         }
     }

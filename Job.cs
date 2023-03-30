39c90f3b22b737e1c7800ffeb855e807d785ebf9
TopBlokeChase <102511226+TopBlokeChase@users.noreply.github.com>
2023-03-30 14:32:28 +1000

    Moved all files into Namespace

--- Job.cs
+++ Job.cs
@@ -1,93 +1,129 @@
 ﻿using System;
 
-
-public class Job : IJob {
-	public Job( uint jobId, uint timeReceived, uint executionTime, uint priority ) {
-		Id = jobId;
-		TimeReceived = timeReceived;
-		ExecutionTime = executionTime;
-		Priority = priority;
-	}
-
-	private uint id;
-	private uint timeReceived;
-	private uint executionTime;
-	private uint priority;
-
-	public uint Id {
-        get
+namespace CAB301_A1_Solution
+{
+    public class Job : IJob
+    {
+        public Job(uint jobId, uint timeReceived, uint executionTime, uint priority)
         {
-			return id;
-		}
-		private set {
-			if (!IsValidId( value ))
-				throw new ArgumentOutOfRangeException( nameof( Id ) );
-			id = value;
-		}
-	}
+            Id = jobId;
+            TimeReceived = timeReceived;
+            ExecutionTime = executionTime;
+            Priority = priority;
+        }
 
-	public uint TimeReceived {
-		get {
-			return timeReceived;
-		}
-		private set {
-            if (!IsTimeReceived(value))
-                throw new ArgumentOutOfRangeException(nameof(Id));
-            timeReceived = value;
-		}
-	}
+        private uint id;
+        private uint timeReceived;
+        private uint executionTime;
+        private uint priority;
 
-	public uint ExecutionTime {
-		get {
-			return executionTime;
-		}
-		private set {
-			if (!IsValidExecutionTime( value ))
-				throw new ArgumentOutOfRangeException( nameof( ExecutionTime ) );
-			executionTime = value;
-		}
-	}
+        public uint Id
+        {
+            get
+            {
+                return id;
+            }
+            private set
+            {
+                if (!IsValidId(value))
+                    throw new ArgumentOutOfRangeException(nameof(Id));
+                id = value;
+            }
+        }
 
-	public uint Priority {
-		get {
-			return priority;
-		}
-		private set {
-			if (!IsValidPriority( value ))
-				throw new ArgumentException( nameof( Priority ) );
-			priority = value;
-		}
-	}
+        public uint TimeReceived
+        {
+            get
+            {
+                return timeReceived;
+            }
+            private set
+            {
+                if (!IsTimeReceived(value))
+                    throw new ArgumentOutOfRangeException(nameof(Id));
+                timeReceived = value;
+            }
+        }
 
-	public static bool IsValidId( uint id ) {
+        public uint ExecutionTime
+        {
+            get
+            {
+                return executionTime;
+            }
+            private set
+            {
+                if (!IsValidExecutionTime(value))
+                    throw new ArgumentOutOfRangeException(nameof(ExecutionTime));
+                executionTime = value;
+            }
+        }
 
-        //To be implemented by students
-        throw new System.NotImplementedException();
+        public uint Priority
+        {
+            get
+            {
+                return priority;
+            }
+            private set
+            {
+                if (!IsValidPriority(value))
+                    throw new ArgumentException(nameof(Priority));
+                priority = value;
+            }
+        }
 
+        public static bool IsValidId(uint id)
+        {
+
+            //To be implemented by students
+            if (id >= 1 && id <= 999)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public static bool IsValidExecutionTime(uint executiontime)
+        {
+
+            //To be implemented by students
+            if (executiontime <= 0)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        public static bool IsValidPriority(uint priority)
+        {
+
+            //To be implemented by students
+            if (id >= 1 && id <= 9)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public static bool IsTimeReceived(uint time)
+        {
+
+            //To be implemented by students
+            if (id > 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Job(jobId: {id}, timeReceived: {timeReceived}, executionTime: {executionTime}, priority: {priority})";
+        }
     }
-
-	public static bool IsValidExecutionTime( uint executiontime ) {
-
-        //To be implemented by students
-        throw new System.NotImplementedException();
-
-    }
-
-    public static bool IsValidPriority( uint priority ) {
-
-        //To be implemented by students
-        throw new System.NotImplementedException();
-
-    }
-
-    public static bool IsTimeReceived(uint time) {
-
-        //To be implemented by students
-        throw new System.NotImplementedException();
-
-    }
-
-    public override string ToString() {
-		return $"Job(jobId: {id}, timeReceived: {timeReceived}, executionTime: {executionTime}, priority: {priority})";
-	}
 }
